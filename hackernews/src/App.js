5d1c0bc7434570a51dc585d88d1d8f9b3846d4ab
Artyom Slivin <slivin.work@gmail.com>
2022-06-28 23:47:11 +0300

    Added pagination logic to queries. Added caching logic.

--- hackernews/src/App.js
+++ hackernews/src/App.js
@@ -2,11 +2,13 @@ import React, { Component } from 'react';
 import './App.css';
 
 const DEFAULT_QUERY = 'redux';
+const DEFAULT_HPP = '10';
 
 const PATH_BASE = 'https://hn.algolia.com/api/v1';
 const PATH_SEARCH = '/search';
 const PARAM_SEARCH = 'query=';
 const PARAM_PAGE = 'page=';
+const PARAM_HPP = 'hitsPerPage=';
 
 const largeColumn = {
 	width: '40%',
@@ -23,20 +25,43 @@ class App extends Component {
 		super(props);
 
 		this.state = {
-			result: null,
+			results: null,
+			searchKey: '',
 			searchTerm: DEFAULT_QUERY,
 		}
 	}
 
+	needsToSearchTopStories = (searchTerm) => {
+		return !this.state.results[searchTerm];
+	}
+
 	fetchSearchTopStories = (searchTerm, page = 0) => {
-		fetch(`${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${searchTerm}&${PARAM_PAGE}${page}`)
+		fetch(`${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${searchTerm}&${PARAM_PAGE}${page}&${PARAM_HPP}${DEFAULT_HPP}`)
 		.then(response => response.json())
 		.then(result => this.setSearchTopStories(result))
 		.catch(error => error);
 	}
 
 	setSearchTopStories = (result) => {
-		this.setState({ result });
+		const { hits, page } = result;
+		const { searchKey, results } = this.state;
+
+		if (results && results[searchKey] && results[searchKey].page <= page) {
+			return;
+		}
+
+		const oldHits = results && results[searchKey] ? results[searchKey].hits : [];
+
+		const updatedHits = [
+			...oldHits,
+			...hits
+		];
+		this.setState({
+			results: {
+				...results,
+				[searchKey]: { hits: updatedHits, page }
+			}
+		});
 	}
 
 	onSearchChange = (event) => {
@@ -45,24 +70,37 @@ class App extends Component {
 
 	onSearchSubmit = (event) => {
 		const { searchTerm } = this.state;
-		this.fetchSearchTopStories(searchTerm);
+		this.setState({ searchKey: searchTerm });
+		if (this.needsToSearchTopStories(searchTerm)) {
+			this.fetchSearchTopStories(searchTerm);
+		}
 		event.preventDefault();
 	}
 
 	onDismiss = (id) => {
-		const updatedHits = this.state.result.hits.filter(item => item.objectID !== id);
-		this.setState({ result: { ...this.state.result, hits: updatedHits } });
+		const { searchKey, results } = this.state;
+		const { hits, page } = results[searchKey];
+		const updatedHits = hits.filter(item => item.objectID !== id);
+		this.setState({
+			results: {
+				...results,
+				[searchKey]: { hits: updatedHits, page }
+			}
+		});
 	}
 
 	componentDidMount = () => {
 		const { searchTerm } = this.state;
+		this.setState({ searchKey: searchTerm });
 		this.fetchSearchTopStories(searchTerm);
 	}
 
 	render() {
-		const { searchTerm, result } = this.state;
+		const { searchTerm, results, searchKey } = this.state;
+		const page = (results && results[searchKey] && results[searchKey].page) || 0;
+		const list = (results && results[searchKey] && results[searchKey].hits) || [];
 
-		console.log('result:', result);
+		console.log('result:', list);
 
 		return (
 			<div className="page">
@@ -75,12 +113,16 @@ class App extends Component {
 						Search
 					</Search>
 				</div>
-				{ result &&
-					<Table
-						list={result.hits}
-						onDismiss={this.onDismiss}
-					/>
-				}
+				<Table
+					list={list}
+					onDismiss={this.onDismiss}
+				/>
+				<div className="interactions">
+					<Button onClick={() => this.fetchSearchTopStories(searchKey, page + 1)}>
+						More
+					</Button>
+				</div>
+
 			</div>
 			);
 	}
