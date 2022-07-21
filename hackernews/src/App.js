79c606287eda8fa7f0d793e5096f19639d793e99
Artyom Slivin <slivin.work@gmail.com>
2022-07-21 16:38:06 +0300

    Moving constants to separate file.

--- hackernews/src/App.js
+++ hackernews/src/App.js
@@ -1,15 +1,15 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 import './App.css';
-
-const DEFAULT_QUERY = 'redux';
-const DEFAULT_HPP = '10';
-
-const PATH_BASE = 'https://hn.algolia.com/api/v1';
-const PATH_SEARCH = '/search';
-const PARAM_SEARCH = 'query=';
-const PARAM_PAGE = 'page=';
-const PARAM_HPP = 'hitsPerPage=';
+import {
+	DEFAULT_QUERY,
+	DEFAULT_HPP,
+	PATH_BASE,
+	PATH_SEARCH,
+	PARAM_SEARCH,
+	PARAM_PAGE,
+	PARAM_HPP
+} from './constants';
 
 const largeColumn = {
 	width: '40%',
