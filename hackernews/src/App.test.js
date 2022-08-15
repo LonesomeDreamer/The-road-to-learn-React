28181530664db83e0d1ad6133ab983290198582d
Artyom Slivin <slivin.work@gmail.com>
2022-08-15 19:22:24 +0300

    Updated tests.

--- hackernews/src/App.test.js
+++ hackernews/src/App.test.js
@@ -1,8 +1,48 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render } from '@testing-library/react';
+import App, { Search, Button, Table } from './App';
 
-test('renders learn react link', () => {
-	render(<App />);
-	const linkElement = screen.getByText(/learn react/i);
-	expect(linkElement).toBeInTheDocument();
+describe('App', () => {
+	test('renders App without crashing', () => {
+		const { asFragment } = render(<App />);
+		expect(asFragment()).toMatchSnapshot();
+	});
+});
+
+describe("Search", () => {
+	it("renders Search crashing", () => {
+		const { asFragment } = render(<Search>Search</Search>);
+		expect(asFragment()).toMatchSnapshot();
+	});
+});
+
+describe("Button", () => {
+	it("renders Button without crashing", () => {
+		const { asFragment } = render(<Button>Give Me More</Button>);
+		expect(asFragment()).toMatchSnapshot();
+	});
+});
+
+describe("Table", () => {
+	const props = {
+		list: [
+			{
+				title: "1",
+				author: "1",
+				num_comments: 1,
+				points: 2,
+				objectID: "y",
+			},
+			{
+				title: "2",
+				author: "2",
+				num_comments: 1,
+				points: 2,
+				objectID: "z",
+			},
+		],
+	};
+	it("renders Table without crashing", () => {
+		const { asFragment } = render(<Table {...props} />);
+		expect(asFragment()).toMatchSnapshot();
+	});
 });
