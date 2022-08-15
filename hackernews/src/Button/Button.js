1ae41c79754da11bc584186999582c906e4ab426
Artyom Slivin <slivin.work@gmail.com>
2022-08-15 19:24:15 +0300

    Added PropTypes to Button component.

--- hackernews/src/Button/Button.js
+++ hackernews/src/Button/Button.js
@@ -1,10 +1,18 @@
+import PropTypes from 'prop-types';
+
 export const Button = ({ onClick, className = "", children }) =>
-	(
-	<button
-		onClick={onClick}
-		className={className}
-		type="button"
-	>
-		{children}
-	</button>
-	)
+(
+<button
+	onClick={onClick}
+	className={className}
+	type="button"
+>
+	{children}
+</button>
+)
+
+Button.propTypes = {
+	onClick: PropTypes.func.isRequired,
+	className: PropTypes.string,
+	children: PropTypes.node.isRequired,
+};
