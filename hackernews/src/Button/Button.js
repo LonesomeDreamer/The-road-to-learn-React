80222f728bfeb062dedf6276b763f4ce566665ab
Artyom Slivin <slivin.work@gmail.com>
2022-08-22 19:11:05 +0300

    Default props for Button component.

--- hackernews/src/Button/Button.js
+++ hackernews/src/Button/Button.js
@@ -1,18 +1,20 @@
 import PropTypes from 'prop-types';
 
-export const Button = ({ onClick, className = "", children }) =>
-(
-<button
-	onClick={onClick}
-	className={className}
-	type="button"
->
-	{children}
-</button>
-)
+export const Button = ({ onClick, className, children }) =>
+	(<button
+		onClick={onClick}
+		className={className}
+		type="button"
+	>
+		{children}
+	</button>);
+
+Button.defaultProps = {
+	className: "",
+};
 
 Button.propTypes = {
 	onClick: PropTypes.func.isRequired,
 	className: PropTypes.string,
-	children: PropTypes.node.isRequired,
+	children: PropTypes.node.isRequired
 };
